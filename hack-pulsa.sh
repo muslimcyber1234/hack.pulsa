929bb14e95ef2944f04c786dca631359400ec3c3
muslimcyber1234 <87059970+muslimcyber1234@users.noreply.github.com>
2021-07-09 05:26:34 -0400

    Add files via upload

---
+++ hack-pulsa.sh
@@ -0,0 +1,109 @@
+#!/system/xbin/bash
+#Clean screen
+clear
+blue='\e[1;34m'
+green='\e[1;32m'
+purple='\[1;35m'
+cyan='\e[1;36m'
+red='\e[1;31m'
+white='\e[1;37m'
+yellow='\e[1;33m'
+sleep 1
+echo "\033[32;1m=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*"
+toilet -f standard -F gay "PULSA FREE"
+echo "\033[31;1m=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*"
+echo ""
+echo "Author : " muslimcyber1234"
+" Team : " muslimcyber1234"
+echo " gmail : " muslimcyber1234@gmail.com"
+" message : "Don't Misuse and Spread :) Because later the sin is on its own:)"
+sleep 1
+echo "ThanksTo:"
+echo "Allmember muslimcyber1234"
+echo "\033[32;1m=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*"
+echo ""
+sleep 3
+echo "pulse hack collection All Operator"
+echo "Use wear +62 #example +62xxx:"
+read p;
+
+echo "number in process"
+sleep 2
+echo "\033[37;1m#10%" 
+sleep 1
+echo "##20%" 
+sleep 1
+echo "###30%" 
+sleep 3
+echo "####40%" 
+sleep 3
+echo "#####50%" 
+sleep 3
+echo "######60%" 
+sleep 5
+echo "\033[31;1mNumber Failed in Process..""\033[32;1mPlease re-enter your number:"
+read p;
+sleep 3
+echo "in the process..."
+sleep 3
+echo "\033[37;1m#10%"
+sleep 2
+echo "##20%"
+sleep 5
+echo "###30%"
+sleep 3
+echo "####40%"
+sleep 3
+echo "#####50%"
+sleep 3
+echo "######60%"
+sleep 3
+echo "#######70%"
+sleep 4
+echo "########80%"
+sleep 3
+echo "#########90%"
+sleep 4
+echo "##########100%"
+sleep 3
+echo "\033[32;1mPlease enter the amount you want:"
+read p;
+sleep 2
+echo "in the process..."
+echo "\033[37;1m#10%"
+sleep 1
+echo "##20%"
+sleep 1
+echo "###30%"
+sleep 3
+echo "####40%"
+sleep 3
+echo "#####50%"
+sleep 3
+echo "######60%"
+sleep 3
+echo "#######70%"
+sleep 4
+echo "########80%"
+sleep 3
+echo "#########90%"
+sleep 4
+echo "##########100%"
+sleep 6
+echo "\033[34;1mAmount You Enter Success...""\033[32;1mPlease Wait 5 Minutes..."
+sleep 2
+sleep 3
+echo "type exit to exit"
+read p;
+sleep 2
+sleep 3
+sleep 4
+
+echo "Message:If This Script Doesn't Work On You It Means There's An Error In This Script"
+sleep 3
+echo "Thank you for those who use this script"
+sleep 2
+echo "ThanksTo:muslimcyber1234"
+sleep 2
+sleep 4
+exit
